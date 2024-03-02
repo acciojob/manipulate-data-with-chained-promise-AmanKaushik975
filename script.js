678fde1eb97abe1823bd82aeeaba320843507c46
AmanKaushik975 <91477500+AmanKaushik975@users.noreply.github.com>
2024-03-02 22:56:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 //your JS code here. If required.
+function processData(arr) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(arr);
+    }, 3000);
+  })
+  .then((arr) => {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        const evenNumbers = arr.filter(num => num % 2 === 0);
+        document.getElementById("output").innerText = evenNumbers.join(", ");
+        resolve(evenNumbers);
+      }, 1000);
+    });
+  })
+  .then((evenNumbers) => {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        const doubledNumbers = evenNumbers.map(num => num * 2);
+        document.getElementById("output").innerText = doubledNumbers.join(", ");
+        resolve(doubledNumbers);
+      }, 2000);
+    });
+  });
+}
+
+// Usage
+const input = [1, 2, 3, 4];
+processData(input);
